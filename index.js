66db350a49d2ce00a3fe52954d47a029bd5b9d13
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-09-20 09:55:14 +0300

    fix

--- index.js
+++ index.js
@@ -35,6 +35,21 @@ const storage = multer.diskStorage({
 //
 const upload = multer({ storage });
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        );
+        return res.status(200).json({});
+    }
+    next();
+});
 app.use(express.json());
 app.use(
     cors({
